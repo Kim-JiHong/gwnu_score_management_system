4fc29f62c1d935b379e0695bb82e7c6537218f13
Kim JiHong <sina2477@gmail.com>
2020-06-25 16:22:03 +0900

    성적 산출 프로그램 완성

--- Get_Score.py
+++ Get_Score.py
@@ -1,23 +1,36 @@
-class Score:
-    total = []
+class Get_Score:
+    tot = []
     avg = []
     excel = []
     rank = []
-
-    def openfile(self):
-        fp = open("score.csv", "r", encoding="utf-8")
+    
+    def Get_Grade(self):
+        fp = open("score.csv","r",encoding = "utf-8")
         self.excel = fp.readlines()
         fp.close()
-
-    def get_score(self):
+                  
         for i in range(len(self.excel)):
-            student = self.excel[i].split(',')
-            kor = int(student[2])
-            eng = int(student[3])
-            math = int(student[4])
-            self.total.append(kor + eng + math)
-            self.avg.append(round(self.total[i] / 3, 2))
+            std = self.excel[i].split(",") 
+            self.tot.append(int(std[2])+int(std[3])+int(std[4]))
+            self.avg.append(round(self.tot[i]/3,2))
         
-scr = Score()
-scr.openfile()
-scr.get_score()
+        for score1 in self.tot:
+            rk = 1
+            for score2 in self.tot:
+                if score1 < score2:
+                    rk = rk + 1
+            self.rank.append(rk)
+
+        fp.close()        
+        l = len(self.excel)
+
+        with open("grade.csv","w",encoding = "utf-8") as wr:
+            for i in range(l):
+                wr.write(self.excel[i].replace('\n','')+',')
+                wr.write(' 총점 // ' + self.tot[i].__str__()+',')
+                wr.write(' 평균 // ' + self.avg[i].__str__()+',')
+                wr.write(' 등수 // ' + self.rank[i].__str__()+'\n')
+
+scr = Get_Score()
+scr.Get_Grade()
+
